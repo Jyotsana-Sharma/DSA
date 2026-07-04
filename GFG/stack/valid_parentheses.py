a008236f4913e85c3904ae8915058349fc91fd41
Jyotsana Sharma <jyotsanasharma212@gmail.com>
2026-07-04 15:28:13 -0400

    valid parentheses

---
+++ GFG/stack/valid_parentheses.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open_to_close = {'[':']','(':')','{':'}'}
+        #creating an empty stack to store the open braces only 
+
+        stack = []
+        for char in s:
+            if(char in open_to_close):
+                #push operation for the open braces
+                stack.append(char)
+            else:
+                if(stack and (open_to_close[stack[-1]]==char) ):
+                    #pop operation for the closed braces
+                    stack.pop()
+                else:
+                    return False
+        if(len(stack)==0):
+            return True
+        else:
+            return False
+obj = Solution()
+s = "()"
+bool_val = obj.isValid(s)
+            
+            
+        
