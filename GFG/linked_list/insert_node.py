373bd7b50612b7391e4b32ec8507c3bf74b8f2a9
Jyotsana Sharma <jyotsanasharma212@gmail.com>
2026-07-16 13:50:51 -0400

    insertion in linked list

--- GFG/linked_list/insert_node.py
+++ GFG/linked_list/insert_node.py
@@ -11,8 +11,22 @@ def insert_at_beginning(head, value):
     new_node.next = head 
     head = new_node
     return head 
-    
 
+def insert_at_end(head, value):
+    current = head
+    new_node = ListNode(value)
+    while current.next:
+        current = current.next
+    
+    current.next = new_node
+    return head
+
+def traverse_linked_list(head):
+    str_print=""
+    while(head):
+        str_print=str_print +"->"+str(head.val) 
+        head = head.next 
+    print(str_print)
 
 
 head = ListNode(10)
@@ -20,11 +34,19 @@ second_node = ListNode(20)
 third_node = ListNode(30)
 
 head.next = second_node
-third_node = None 
+third_node.next = None 
 
 head = insert_at_beginning(head, 100)
 print(head.val)
 head = insert_at_beginning(head, 200)
 print(head.val)
+traverse_linked_list(head)
 
-    
+
+head = insert_at_end(head, 400)
+head = insert_at_end(head, 500)
+
+traverse_linked_list(head)
+
+
+
