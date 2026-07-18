7adcda37fc70b9ac37abb9cdf5857be7dcc69561
Jyotsana Sharma <jyotsanasharma212@gmail.com>
2026-07-18 16:48:30 -0400

    added insertion at specific index

--- GFG/linked_list/insert_node.py
+++ GFG/linked_list/insert_node.py
@@ -28,6 +28,29 @@ def traverse_linked_list(head):
         head = head.next 
     print(str_print)
 
+def insert_at_specific_idx(head,val,position):
+    new_node = ListNode(val)
+  
+    if position == 0:
+        new_node.next = head 
+        return new_node
+    current  = head
+
+    #loop through all the elements till the position-1 i will go from 0 till 2 and will stop at idx 1
+    for i in range(position-1):
+        current = current.next
+    
+    if current is None:
+        return head
+    
+    #we have current pointer pointing to index 1 
+    #we will save the rest of the linked list to the new node's next 
+    new_node.next = current.next
+
+    #Current index 1 holds value of 100 so it's next value should be 25
+    current.next = new_node 
+    return head    
+
 
 head = ListNode(10)
 second_node = ListNode(20)
@@ -37,16 +60,21 @@ head.next = second_node
 third_node.next = None 
 
 head = insert_at_beginning(head, 100)
-print(head.val)
+print(head.val) #Prints 100
 head = insert_at_beginning(head, 200)
-print(head.val)
-traverse_linked_list(head)
+print(head.val) #Prints 200
+traverse_linked_list(head) #Prints (->200->100->10->20)
 
 
 head = insert_at_end(head, 400)
 head = insert_at_end(head, 500)
 
-traverse_linked_list(head)
+traverse_linked_list(head) #Prints (->200->100->10->20->400->500)
+
+insert_val = 25 
+insert_val_idx = 2
+insert_at_specific_idx(head,insert_val,insert_val_idx)
+traverse_linked_list(head) # Prints (->200->100->10->25->20->400->500)
 
 
 
