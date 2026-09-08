adccaef3924efd73d54ed151b727fadf3441aa96
Jyotsana Sharma <jyotsanasharma212@gmail.com>
2026-09-08 00:27:59 -0400

    Adding LRU Cache

--- GFG/linked_list/doubly_linked_list.py
+++ GFG/linked_list/doubly_linked_list.py
@@ -4,9 +4,7 @@ class DoubleNode:
         self.value = value 
         self.prev = None 
         self.next = None 
-        
-     
-
+         
 class LRUCache:
 
     def __init__(self,capacity):
@@ -19,8 +17,50 @@ class LRUCache:
         self.tail.prev = self.head 
     
     def remove(self,node):
-        self.node = node
-        self.node.prev=self.node.next
+        #connect left
+        node.prev.next = node.next
+        #connect right
+        node.next.prev = node.prev
+    
+    def insert_at_front(self, node):
+        #first save the connections of the new node 
+        node.next = self.head.next
+        node.prev = self.head
+
+        #connect it from left and right both ends/nodes
+        self.head.next.prev = node 
+        self.head.next = node 
+
+    def get(self, key):
+        if key not in self.cache:
+            return -1 
+        node = self.cache[key]
+        self.remove(node)
+        self.insert_at_front(node)
+        return node.value
+    
+    def put(self,key,value):
+        #self.cache[key] stores the node not the value 
+        if key in self.cache.keys():
+            node = self.cache[key]
+            node.value = value
+            self.remove(node)
+            self.insert_at_front(node)
+        else:
+            node = DoubleNode(key,value)
+            self.cache[key] = node 
+            self.insert_at_front(node)
+            if(len(self.cache) > self.capacity):
+                last_node = self.tail.prev 
+                del self.cache[last_node.key]
+                self.remove(last_node)
+
+            
+
+
+    
+
+
         
 
 
